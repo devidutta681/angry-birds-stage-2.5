18736ed5b16eb436d33c0495e21b77ea228820d9
devidutta681 <suprava7107@gmail.com>
2021-05-15 12:50:22 +0530

    added a new platform

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    platform = new Ground(100,400,400,450)
 }
 
 function draw(){
@@ -59,4 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
